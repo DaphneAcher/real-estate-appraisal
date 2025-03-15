5707886988b2dd08f9a61599205adee938a4ce98
Daphne Acher <191528512+DaphneAcher@users.noreply.github.com>
2025-03-15 16:35:16 -0400

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/home1.svg'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -11,16 +10,9 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-      <Router>
-          <Navbar />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/contact" element={<Contact />} />
-          </Routes>
-      </Router>
+      <div>
+        <h1>Wachstetter Appraisals</h1>
+      </div>
   );
 }
 

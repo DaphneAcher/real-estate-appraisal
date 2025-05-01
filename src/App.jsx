64945acee63c61858b5db29730db5101093d55a9
Daphne Acher <191528512+DaphneAcher@users.noreply.github.com>
2025-05-01 18:24:49 -0400

    scrapped all CSS and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/home1.svg'
-import Navbar from "./Components/Header/Navbar";
-import Home from "./Components/Home/Home";
 
 
 function App() {
   return (
       <div>
-        <Navbar />
-        <Home />
       </div>
   );
 }

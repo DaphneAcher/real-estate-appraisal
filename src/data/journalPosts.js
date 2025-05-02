2a1a5aab46fcb1eea02b08d6ff43c3b3daf9ae16
Daphne Acher <191528512+DaphneAcher@users.noreply.github.com>
2025-05-01 21:07:40 -0400

    added journalPost.js in order to easily test the journal

---
+++ src/data/journalPosts.js
@@ -0,0 +1,12 @@
+const journalPosts=[
+    {
+        id: 1,
+        title: "Three methods used in commercial real estate appraisal",
+        author: "Tom Wachstetter",
+        content: "here are the three methods",
+        date: "5/21/24",
+        slug: "commercial-appraisal-methods"
+    }
+];
+
+export {journalPosts};

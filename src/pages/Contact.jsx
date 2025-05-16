badba534ba0aa7e7d5362819d0ccd1c8d48db76f
Daphne Acher <191528512+DaphneAcher@users.noreply.github.com>
2025-05-16 15:09:26 -0400

    contact skeleton

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,31 @@
 export default function Contact() {
   return (
-    <div className="p-20">
-      <h1 className="text-3xl font-bold">Contact</h1>
-    </div>
+    <>
+    <section className="py-12 px-6 max-w-4xl mx-auto">
+      <h2 className="text-3xl font-bold text-center mb-6">Call Us</h2>
+       <div className="text-center mb-12 space-y-2 text-orange-600 font-medium">
+        <p>Phone: 954.791.4667</p>
+        <p>Fax: 954.791.4693</p>
+       </div>
+
+       <h2 className="text-2xl font-bold text-center mb-6">Send us a messege</h2>
+      <form className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+        <input type="text" placeholder="Your Name *" className="border p-3 rounded" required />
+        <input type="tel" placeholder="Phone Number *" className="border p-3 rounded" required />
+        <input type="email" placeholder="Email Address *" className="border p-3 rounded md:col-span-1" required />
+        <input type="text" placeholder="Property Address *" className="border p-3 rounded md:col-span-1" required />
+        <textarea placeholder="Services Needed... *" className="border p-3 rounded md:col-span-2 h-32 resize-none" required />
+      </form>
+
+      <div className="text-center">
+        <button className="bg-orange-500 hover:bg-orange-600 text-white font-semibold px-6 py-3 rounded-md">
+          Send Message
+        </button>
+      </div>
+      
+    </section>
+
+    
+    </>
   );
 }
